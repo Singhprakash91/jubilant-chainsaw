078feac4884343b4ea3943a8b3307d44e7256079
Prakash Singh <satya.ps@flipkart.com>
2021-11-21 23:36:15 +0530

    adding yaml and py file to test git cases

--- python_yaml.py
+++ python_yaml.py
@@ -1,7 +1,8 @@
+# pip install pyyaml
 import yaml
 
 # Reading a YAML file
-with open('prexo.yml') as f:
+with open('all_datatypes.yml') as f:
     yaml_contents = yaml.load_all(f, Loader=yaml.FullLoader)
     for yaml_content in yaml_contents:
         for key, value in yaml_content.items():
