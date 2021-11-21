0ec948e1b1be0c8601b768d7e82b4dcbe9b4f4f3
Prakash Singh <satya.ps@flipkart.com>
2021-11-21 19:19:59 +0530

    test YAML

---
+++ python_yaml.py
@@ -0,0 +1,8 @@
+import yaml
+
+# Reading a YAML file
+with open('prexo.yml') as f:
+    yaml_contents = yaml.load_all(f, Loader=yaml.FullLoader)
+    for yaml_content in yaml_contents:
+        for key, value in yaml_content.items():
+            print(f"{key}: {value}")
