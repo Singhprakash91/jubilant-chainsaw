604f35acaf523a68ae0e7306b62f1bd2edf5d4ce
Prakash Singh <satya.ps@flipkart.com>
2021-11-26 01:49:31 +0530

    added hello to repo

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+print("hello world")
+a = input("enter a number\n")
+print("your number is: ",a)
+
+
+def func1():
+    #nkbnksbvkjvsxz
+    #jv bcvxjb b
+    # jkbkfbdsfkvxz
+    print("something")
+    
+    
